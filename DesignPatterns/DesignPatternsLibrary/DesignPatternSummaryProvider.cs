3de75eac7fbb095adddf462941aa63c268ea4b9d
Shubham Jain <shubhamjain993@gmail.com>
2020-04-22 21:45:54 +0530

    Removed dead code 1
    Removed all unused 'using' from all files

--- DesignPatterns/DesignPatternsLibrary/DesignPatternSummaryProvider.cs
+++ DesignPatterns/DesignPatternsLibrary/DesignPatternSummaryProvider.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatternsLibrary
+﻿namespace DesignPatternsLibrary
 {
     public class DesignPatternSummaryProvider
     {
--- DesignPatterns/DesignPatternsLibrary/SimpleFactory/PizzaStore/Pizzas/VeggiePizza.cs
+++ DesignPatterns/DesignPatternsLibrary/SimpleFactory/PizzaStore/Pizzas/VeggiePizza.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatternsLibrary.SimpleFactory.PizzaStore
+﻿namespace DesignPatternsLibrary.SimpleFactory.PizzaStore
 {
     public class VeggiePizza : Pizza
     {
